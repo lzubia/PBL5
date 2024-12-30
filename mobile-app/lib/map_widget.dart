a94e3fad23137054bd65f150073b30952d18a1eb
Alain <alain.ordonez@alumni.mondragon.edu>
2024-12-30 20:07:24 +0100

    feat: implement destination search and display directions in map widget

--- mobile-app/lib/map_widget.dart
+++ mobile-app/lib/map_widget.dart
@@ -25,6 +25,7 @@ class OrderTrackingPageState extends State<MapWidget> {
   LatLng? destination;
 
   List<LatLng> polylineCoordinates = [];
+  List<String> _instructions = [];
   LocationData? currentLocation;
   StreamSubscription<LocationData>? locationSubscription;
 
@@ -78,6 +79,25 @@ class OrderTrackingPageState extends State<MapWidget> {
     super.dispose();
   }
 
+  Future<void> _searchDestination() async {
+    final apiKey = dotenv.env['GEOCODING_API_KEY'];
+    final response = await http.get(
+      Uri.parse(
+          'https://maps.googleapis.com/maps/api/geocode/json?address=${_destinationController.text}&key=$apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final location = data['results'][0]['geometry']['location'];
+      setState(() {
+        destination = LatLng(location['lat'], location['lng']);
+        getPolyPoints();
+      });
+    } else {
+      throw Exception('Failed to load location');
+    }
+  }
+
   void getPolyPoints() async {
     PolylinePoints polylinePoints = PolylinePoints();
 
@@ -100,6 +120,7 @@ class OrderTrackingPageState extends State<MapWidget> {
       if (mounted) {
         setState(
           () {
+            polylineCoordinates.clear();
             for (PointLatLng point in result.points) {
               polylineCoordinates.add(
                 LatLng(
@@ -110,28 +131,39 @@ class OrderTrackingPageState extends State<MapWidget> {
             }
           },
         );
+        _fetchAndDisplayDirections();
       }
     } else {
       print('No points found');
     }
   }
 
-  Future<void> _searchDestination() async {
-    final apiKey = dotenv.env['GEOCODING_API_KEY'];
-    final response = await http.get(
-      Uri.parse(
-          'https://maps.googleapis.com/maps/api/geocode/json?address=${_destinationController.text}&key=$apiKey'),
-    );
+  Future<void> _fetchAndDisplayDirections() async {
+    try {
+      final instructions = await fetchDirections(
+        '${currentLocation?.latitude},${currentLocation?.longitude}',
+        '${destination?.latitude},${destination?.longitude}',
+        dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '',
+      );
+      setState(() {
+        _instructions = instructions;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<List<String>> fetchDirections(
+      String origin, String destination, String apiKey) async {
+    final response = await http.get(Uri.parse(
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&mode=walking&key=$apiKey'));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      final location = data['results'][0]['geometry']['location'];
-      setState(() {
-        destination = LatLng(location['lat'], location['lng']);
-        getPolyPoints();
-      });
+      final steps = data['routes'][0]['legs'][0]['steps'] as List;
+      return steps.map((step) => step['html_instructions'] as String).toList();
     } else {
-      throw Exception('Failed to load location');
+      throw Exception('Failed to load directions');
     }
   }
 
@@ -192,6 +224,16 @@ class OrderTrackingPageState extends State<MapWidget> {
                       _controller.complete(mapController);
                     },
                   ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _instructions.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_instructions[index]),
+                );
+              },
+            ),
           ),
         ],
       ),
